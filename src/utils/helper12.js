ad88167bf96e6b4e42a50a4e7fc9e3bbddb9acdf
Barfi177 <alabarfi99@gmail.com>
2025-08-14 14:49:04 +0600

    Add src/utils/helper12.js - Generated by Adorable AI

---
+++ src/utils/helper12.js
@@ -0,0 +1,14 @@
+export const courses = [
+  {
+    id: 1,
+    title: 'Intro to React',
+    description: 'Learn React basics',
+    lessons: ['JSX', 'Components', 'State & Props']
+  },
+  {
+    id: 2,
+    title: 'Advanced JavaScript',
+    description: 'Deep dive into JS',
+    lessons: ['Closures', 'Prototypes', 'Async']
+  }
+];
